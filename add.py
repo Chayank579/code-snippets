f7e3a7502d300c60e0e2fbfca768d502320dfbeb
579 <creddymusku_cse230579@mgit.ac.in>
2025-11-04 13:39:38 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,11 @@
+# add.py
+
+# Ask the user for two numbers
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {sum}")
