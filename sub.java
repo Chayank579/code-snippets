2b398a193506cd74deae72200eee2e9c2f951888
579 <creddymusku_cse230579@mgit.ac.in>
2025-11-04 13:41:01 +0530

    Add Sub.java to perform subtraction of two numbers

---
+++ sub.java
@@ -0,0 +1,15 @@
+// Sub.java
+
+public class Sub {
+    public static void main(String[] args) {
+        // Static input values
+        double num1 = 15.5;
+        double num2 = 7.2;
+
+        // Subtract the numbers
+        double result = num1 - num2;
+
+        // Display the result
+        System.out.println("The difference between " + num1 + " and " + num2 + " is " + result);
+    }
+}
